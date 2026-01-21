b8fc6c83475c24cdf2642d6c0aa02b7c74a30e81
desireelefundes <desireelefundes@gmail.com>
2026-01-21 17:52:54 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaRhRdi/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class SistemaRhRdi
+{
+
+    public class Funcionario
+    {
+        // Atributos:
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int CPF { get; set; }
+        public string Email { get; set; }
+
+        // Construtor:
+        public Funcionario(int id, string nome, int cpf, string email)
+        {
+            Id = id;
+            Nome = nome;
+            CPF = cpf;
+            Email = email;
+        }
+
+    }
+
+}
